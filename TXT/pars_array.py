230f50ff16f5a44818991e1ad59e59c71afee391
lacersx <williaanes@gmail.com>
2024-09-26 10:02:39 +0700

    Update and rename array.py to pars_array.py

--- TXT/pars_array.py
+++ TXT/pars_array.py
@@ -2,7 +2,10 @@ def parsing_array(data):
     # Fungsi untuk memparsing data Array
     if ',' in data:
         array = data.split(',')
-    else:
+    elif '\n' in data:
         array = data.split('\n')
-    
+    else:
+        array = [data] 
+        
+    array = [item.strip() for item in array]
     return array
