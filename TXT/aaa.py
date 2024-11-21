59728bc5929a837114968cbdbc6eaf66286e4a52
lacersx <williaanes@gmail.com>
2024-11-21 10:04:05 +0700

    calendar

--- TXT/aaa.py
+++ TXT/aaa.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from datetime import datetime
-from tkcalendar import calendar
+from tkcalendar import Calendar
 
 class Baca:
     @staticmethod
